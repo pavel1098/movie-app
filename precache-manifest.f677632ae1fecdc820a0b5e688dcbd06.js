84ae8cf786faaa47e854f15f9865490bab01677c
pavel <pmykhaleiko@gmail.com>
2020-01-15 19:36:40 +0200

    Updates

---
+++ precache-manifest.f677632ae1fecdc820a0b5e688dcbd06.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c858c68dc141779dc80b547185a7e4",
+    "url": "/movie-app/index.html"
+  },
+  {
+    "revision": "1cdd4f0a2c5db0c17297",
+    "url": "/movie-app/static/js/2.a1ee46ba.chunk.js"
+  },
+  {
+    "revision": "51675594b9c5d603232f1f81d197fa7f",
+    "url": "/movie-app/static/js/2.a1ee46ba.chunk.js.LICENSE"
+  },
+  {
+    "revision": "019343ccf4443fd7175a",
+    "url": "/movie-app/static/js/main.c6964f79.chunk.js"
+  },
+  {
+    "revision": "60f14217498504fcfb6a",
+    "url": "/movie-app/static/js/runtime-main.8e14a1ed.js"
+  }
+]);
